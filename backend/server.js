8b884e71312dae493c32a82ec73e5bafc5e63ac6
Tan Vu <duytanvu@icloud.com>
2020-12-31 10:49:38 -0500

    Fetch products from backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotevn = require('dotenv');
+
+const products = require('./data/products');
+const app = express();
+dotevn.config();
+
+const { PORT = 5000, NODE_ENV } = process.env;
+
+app.get('', (req, res) => {
+  res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(p => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
+);
