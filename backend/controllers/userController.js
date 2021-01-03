0ffdc5cc57626c5864450b766080827252e6bfa8
Tan Vu <duytanvu@icloud.com>
2021-01-03 14:01:38 -0500

    Implement get user profile functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -86,6 +86,7 @@ const getUserProfile = asyncHandler(async (req, res) => {
  */
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
+
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
@@ -93,14 +94,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       user.password = req.body.password;
     }
 
-    const updateUser = await user.save();
+    const updatedUser = await user.save();
 
     res.json({
-      _id: updateUser._id,
-      name: updateUser.name,
-      email: updateUser.email,
-      isAdmin: updateUser.isAdmin,
-      token: generateToken(updateUser._id),
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
     });
   } else {
     res.status(404);
