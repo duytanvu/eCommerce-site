3cdf9d9ea977cd9116f32534a33e6160c0e0ba11
Tan Vu <duytanvu@icloud.com>
2021-01-05 17:51:46 -0500

    Add edit product functionality for admin panel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import {
   getProduct,
   getProductById,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProduct);
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
