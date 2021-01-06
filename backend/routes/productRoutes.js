7adc52054cd705b1ab64198945e75e980ab93f28
Tan Vu <duytanvu@icloud.com>
2021-01-05 19:22:02 -0500

    Add edit and add new product controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,10 +4,16 @@ import {
   getProduct,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProduct);
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router.route('/').get(getProduct).post(protect, admin, createProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
