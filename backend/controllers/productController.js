19ad1922165e596f22a7317ee35d4765ab651cb7
Tan Vu <duytanvu@icloud.com>
2021-01-07 14:01:20 -0500

    Add paginition for products screen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -8,6 +8,9 @@ import Product from '../models/productModel.js';
  * @access      Public
  */
 const getProduct = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+  const page = +req.query.pageNumber || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -17,8 +20,11 @@ const getProduct = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 /**
