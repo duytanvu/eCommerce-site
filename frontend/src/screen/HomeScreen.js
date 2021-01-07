8ce73c46196394de6fa4dd26e17b7b90cfe0d5f7
Tan Vu <duytanvu@icloud.com>
2021-01-07 16:55:56 -0500

    Prepare for deployment to Heroku

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
 import Product from '../components/Product';
