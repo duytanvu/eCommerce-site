32bf6df32189ba3de2cda2e303c857c041681371
Tan Vu <duytanvu@icloud.com>
2021-01-08 22:11:11 -0500

    Check for userInfo in product list screen

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -20,7 +20,7 @@ const productSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      required: true,
+      required: false,
       ref: 'User',
     },
     name: {
